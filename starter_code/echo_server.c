929e3bb509b13f52a22f1661db7d2e9ef9bb612d
Yunie Mao <ym224@cornell.edu>
2017-09-25 22:04:12 -0400

    add echo_server

--- starter_code/echo_server.c
+++ starter_code/echo_server.c
@@ -1,15 +1,8 @@
-/******************************************************************************
-* echo_server.c                                                               *
-*                                                                             *
-* Description: This file contains the C source code for an echo server.  The  *
-*              server runs on a hard-coded port and simply write back anything*
-*              sent to it by connected clients.  It does not support          *
-*              concurrent clients.                                            *
-*                                                                             *
-* Authors: Athula Balachandran <abalacha@cs.cmu.edu>,                         *
-*          Wolf Richter <wolf@cs.cmu.edu>                                     *
-*                                                                             *
-*******************************************************************************/
+/*
+ * Echo-based server.
+ * Modified from Beej's guide and tutorial on
+ * http://forum.codecall.net/topic/64205-concurrent-tcp-server-using-select-api-in-linux-c/
+ */
 
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -32,17 +25,18 @@ int close_socket(int sock) {
 
 int main(int argc, char *argv[]) {
     fd_set master, read_fds; // master and temp list for select()
-    int maxfd, sock, client_sock; //maximum file descriptor #, listening socket descriptor and newly accepted socket descriptor
-    int i;
+    int sock, client_sock; //listening socket descriptor and newly accepted socket descriptor
+    int i, j, k;
+    int client[FD_SETSIZE], nready;
+    int maxfd, max_idx;
+    int http_port;
     ssize_t readret; /*ssize_t = the sizes of blocks that can be read or written in a single operation*/
     socklen_t cli_size; /*socklen_t = an integral type of at least 32 bits*/
     struct sockaddr_in addr, cli_addr; /*struct = a user defined data type that allows to combine data items of different kind*/
-    //struct addrinfo hints, *serviceinfo, *p;
     char buf[BUF_SIZE];
-    int nbytes;
+    int yes = 1;
 
-    FD_ZERO(&master);
-    FD_ZERO(&read_fds);
+    //http_port = atoi(argv[1]);
 
     fprintf(stdout, "----- Echo Server -----\n");
 
@@ -58,6 +52,8 @@ int main(int argc, char *argv[]) {
     addr.sin_family = AF_INET;
     addr.sin_port = htons(ECHO_PORT); /*htons makes sure that the numbers are stored in memory in network bytes order*/
     addr.sin_addr.s_addr = INADDR_ANY; /*????????INADDR_ANY?????*/
+
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
     /* servers bind sockets to ports---notify the OS they accept connections */
     if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) /*bind(int sockfd, struct sockaddr *my_addr,int addrlen); function assigns a local protocol address to a socket. The protocol address is the combination of either a IPv4 or IPv6 address, along with a 16-bit port number. */
@@ -79,58 +75,70 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    FD_SET(sock, &master); // add the current listening descriptor to the monitored set
+    for (i=0; i<FD_SETSIZE; i++) {
+        client[i] = -1;
+    }
+    max_idx = -1;
     maxfd = sock; //initialize maxfd
+    FD_ZERO(&master); // initialize descriptor to empty
+    FD_SET(sock, &master); // add the current listening descriptor to the monitored set
 
 
     /* finally, loop waiting for input and then write it back */
     while (1) {
         read_fds = master;
-        if (select(maxfd + 1, &read_fds, NULL, NULL, NULL) == -1) {
+        nready = select(maxfd + 1, &read_fds, NULL, NULL, NULL);
+        if ( nready == -1) {
             perror("select");
             exit(4);
         }
 
-        for (i = 0; i <= maxfd; i++) {
-            if (FD_ISSET(i, &read_fds)) {
-                if (i == sock) {
-                    cli_size = sizeof(cli_addr);
-                    client_sock = accept(sock, (struct sockaddr *) &cli_addr, &cli_size);
-                    /*
-                    accept(int socket, struct sockaddr *restrict address,
-                    socklen_t *restrict address_len);
-                    extrac the first connection on the queue of pending connections,
-                    create a new socket with the same socket type protocol
-                    */
-                    if (client_sock == -1) {
-                        perror("accept");
-                    } else {
-                        FD_SET(client_sock, &master); // add to master set
-                        if (client_sock > maxfd) {
-                            maxfd = client_sock;
-                        }
-                        printf("selectserver: new connection from %s on socket %d\n", inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr*)&cli_addr), remoteIP, INET6_ADDRSTRLEN), client_sock);
-                    }
-                }
-                else {
-                    readret = 0;
-                    // handle data from a client
-                    while ((readret = read(i, buf, sizeof buf)) >= 1) {
+        if (FD_ISSET(sock, &read_fds)) {
+            cli_size = sizeof(cli_addr);
+            client_sock = accept(sock, (struct sockaddr *) &cli_addr, &cli_size);
+            nready--;
+            for (j = 0; j <= FD_SETSIZE; j++) {
+                if (client[j] < 0) {
+                    client[j] = client_sock;
 
-                        if (send(i, buf, readret, 0) != readret) {
-                            close_socket(client_sock);
+                    FD_SET(client_sock, &master);
+                    if (client_sock > maxfd) {
+                        maxfd = client_sock;
+                    }
+                    if (j > max_idx) {
+                        max_idx = j;
+                    }
+                    break;
+                }
+            }
+        }
+        else {
+            for (k = 0; (k <= max_idx && nready > 0); k++) {
+                if (client[k] > 0 && FD_ISSET(client[k], &read_fds)) {
+                    nready--;
+                    if ((readret = read(client[k], buf, BUF_SIZE)) > 1) {
+                        printf("Server received %d bytes data on %d\n",
+                               (int)readret, client[k]);
+
+                        if (send(client[k], buf, readret, 0) != readret) {
+                            close_socket(client[k]);
                             close_socket(sock);
                             fprintf(stderr, "Error sending to client.\n");
-                            return EXIT_FAILURE;
+                            exit(EXIT_FAILURE);
                         }
+                        printf("Server sent %d bytes data to %d\n",
+                               (int)readret, client[k]);
                         memset(buf, 0, BUF_SIZE);
-                        close(i); // bye!
-                        FD_CLR(i, &master); // remove from master set
-                    }
-                    // got error or connection closed by client
-                    if (readret == -1) {
-                        // connection closed
-                        printf("selectserver: socket %d hung up\n", i);
+                    } else {
+                        if (readret == 0) {
+                            printf("serve_clients: socket %d hung up\n", client[k]);
+                        }
+                        else {
+                            fprintf(stderr, "serve_clients: recv return -1\n");
+                        }
+                        close_socket(client[k]);
+                        FD_CLR(client[k], &master);
+                        client[k] = -1;
                     }
                 }
             }
