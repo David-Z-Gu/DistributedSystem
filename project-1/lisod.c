7f71dbb677470bbec27570681c90e61070dd4985
Yunie Mao <ym224@cornell.edu>
2017-09-30 19:27:08 -0400

    - move functions to process http request into new file - update makefile

--- project-1/lisod.c
+++ project-1/lisod.c
@@ -14,23 +14,10 @@
 #include <fcntl.h>
 #include "log.h"
 #include "parse.h"
+#include "process_request.h"
 
 #define ECHO_PORT 9999
 #define BUF_SIZE 4096
-#define HTTP_RESPONSE_SIZE 200000
-#define MAX_FILE_BUF_SIZE 100000
-
-char* HTTP_VERSION = "HTTP/1.1 ";
-char* STATUS_200 = "200 OK\r\n";
-char* STATUS_204 = "204 NO CONTENT\r\n";
-char* STATUS_400 = "400 BAD REQUEST\r\n";
-char* STATUS_404 = "404 NOT FOUND\r\n";
-char* STATUS_405 = "405 METHOD NOT ALLOWED\r\n";
-char* STATUS_411 = "411 Length REQUIRED\r\n";
-char* STATUS_415 = "415 UNSUPPORTED MEDIA TYPE\r\n";
-char* STATUS_500 = "500 INTERNAL SERVER ERROR\r\n";
-char* STATUS_501 = "501 NOT IMPLEMENTED\r\n";
-char* STATUS_505 = "505 HTTP VERSION NOT SUPPORTED\r\n";
 
 int close_socket(int sock) {
     log_write("closing sock %d\n", sock);
@@ -39,164 +26,6 @@ int close_socket(int sock) {
         return 1;
     }
     return 0;
-}
-
-void get_content_type(char *file_ext, char *content_type) {
-    if (strstr(file_ext, ".html")) {
-        strcpy(content_type, "text/html");
-    }
-    else if (strstr(file_ext, ".css")) {
-        strcpy(content_type, "text/css");
-    }
-    else if (strstr(file_ext, ".png")) {
-        strcpy(content_type, "image/png");
-    }
-    else if (strstr(file_ext, ".jpeg")) {
-        strcpy(content_type, "image/jpeg");
-    }
-    else if (strstr(file_ext, ".gif")) {
-        strcpy(content_type, "image/gif");
-    }
-    else {
-        strcpy(content_type, "application/octet-stream");
-    }
-}
-
-int check_file_access(char *file_path, char *response) {
-    if (access(file_path, F_OK) == -1) {
-        log_write("cannot access file at %s\n", file_path);
-        // return not found
-        strcat(response, HTTP_VERSION);
-        strcat(response, STATUS_404);
-        strcat(response, "\r\n");
-    }
-    // open uri w readyonly
-    int file = open(file_path, O_RDONLY);
-    if (file == -1) {
-        log_write("can't open file at %s\n", file_path);
-        strcat(response, HTTP_VERSION);
-        strcat(response, STATUS_500);
-        strcat(response, "\r\n");
-    }
-    return file;
-}
-
-void process_head(Request * request, char * response){
-    char file_path[BUF_SIZE], content_type[BUF_SIZE];
-    size_t content_length;
-
-    fprintf(stdout, "req uri %s\n", request->http_uri);
-    // get request uri to get location of file
-    strcat(file_path, "www");
-    strcat(file_path, request->http_uri);
-
-    int file = check_file_access(file_path, response);
-    if (file < 0) {
-        return;
-    }
-
-    char nbytes[MAX_FILE_BUF_SIZE];
-    // get content length from reading file
-    content_length = read(file, nbytes, sizeof(nbytes));
-
-    // get content type based on uri
-    get_content_type(request->http_uri, content_type);
-
-    // construct response
-    //sprintf(response, "HTTP/1.1 200 OK\r\n");
-    strcat(response, HTTP_VERSION);
-    strcat(response, STATUS_200);
-    //sprintf(response, "%sDate: %s\r\n", response, dbuf);
-    sprintf(response, "%sServer: Liso/1.0\r\n", response);
-    //if (is_closed) sprintf(buf, "%sConnection: close\r\n", response);
-    sprintf(response, "%sContent-Length: %ld\r\n", response, content_length);
-    sprintf(response, "%sContent-Type: %s\r\n", response, content_type);
-    //sprintf(buf, "%sLast-Modified: %s\r\n\r\n", buf, tbuf);
-    strcat(response, "\r\n");
-
-    memset(file_path, 0, BUF_SIZE);
-    memset(nbytes, 0, MAX_FILE_BUF_SIZE);
-    memset(content_type, 0, BUF_SIZE);
-}
-
-void process_get(Request * request, char * response){
-    char file_path[BUF_SIZE], content_type[BUF_SIZE];
-    size_t content_length;
-
-    fprintf(stdout, "req uri %s\n", request->http_uri);
-    // get request uri to get location of file
-    strcat(file_path, "www");
-    strcat(file_path, request->http_uri);
-
-    int file = check_file_access(file_path, response);
-    if (file < 0) {
-        return;
-    }
-
-    char nbytes[MAX_FILE_BUF_SIZE];
-    // get content length from reading file
-    content_length = read(file, nbytes, sizeof(nbytes));
-
-    // get content type based on uri
-    get_content_type(request->http_uri, content_type);
-
-    // construct response
-    strcat(response, HTTP_VERSION);
-    strcat(response, STATUS_200);
-    //sprintf(response, "%sDate: %s\r\n", response, dbuf);
-    sprintf(response, "%sServer: Liso/1.0\r\n", response);
-    //if (is_closed) sprintf(buf, "%sConnection: close\r\n", response);
-    sprintf(response, "%sContent-Length: %ld\r\n", response, content_length);
-    sprintf(response, "%sContent-Type: %s\r\n", response, content_type);
-    //sprintf(buf, "%sLast-Modified: %s\r\n\r\n", buf, tbuf);
-    strcat(response, "\r\n");
-
-    strcat(response, nbytes);
-
-    memset(file_path, 0, BUF_SIZE);
-    memset(nbytes, 0, MAX_FILE_BUF_SIZE);
-    memset(content_type, 0, BUF_SIZE);
-}
-
-void process_post(Request * request, char * response){
-    char file_path[BUF_SIZE];
-
-    fprintf(stdout, "req uri %s\n", request->http_uri);
-    // get request uri to get location of file
-    strcat(file_path, "www");
-    strcat(file_path, request->http_uri);
-
-    int file = check_file_access(file_path, response);
-    if (file < 0) {
-        return;
-    }
-
-    char * content_length;
-    // get content-length from header
-    for (int i=0; i< request->header_count; i++){
-        if (strcmp(request->headers[i].header_name, "Content-Length") || strcmp(request->headers[i].header_name, "content-length")){
-            content_length = request->headers[i].header_value;
-        }
-    }
-
-    if (content_length == NULL) {
-        // return 411
-        strcat(response, HTTP_VERSION);
-        strcat(response, STATUS_411);
-        strcat(response, "\r\n");
-        return;
-    }
-
-    // process body of post??
-
-    printf("successful post");
-    strcat(response, HTTP_VERSION);
-    strcat(response, STATUS_200);
-    sprintf(response, "%sServer: Liso/1.0\r\n", response);
-    sprintf(response, "%sContent-Length: %s\r\n", response, content_length);
-    strcat(response, "\r\n");
-
-    memset(file_path, 0, BUF_SIZE);
 }
 
 int main(int argc, char *argv[]) {
@@ -223,7 +52,7 @@ int main(int argc, char *argv[]) {
     /* all networked programs must create a socket */
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) /*socket(int domain, int type, int protocol); call creates an endpoint for communication and return a descriptor*/
     {
-        fprintf(stderr, "Failed creating socket.\n");
+        log_write("Failed creating socket.\n");
         return EXIT_FAILURE;
     }
 
@@ -237,7 +66,7 @@ int main(int argc, char *argv[]) {
     if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0)
     {
         close_socket(sock);
-        fprintf(stderr, "Failed binding socket.\n");
+        log_write("Failed binding socket.\n");
         return EXIT_FAILURE;
     }
 
@@ -246,7 +75,7 @@ int main(int argc, char *argv[]) {
     /*backlog - the number of allowed connections.*/
     {
         close_socket(sock);
-        fprintf(stderr, "Error listening on socket.\n");
+        log_write("Error listening on socket.\n");
         return EXIT_FAILURE;
     }
 
@@ -293,48 +122,14 @@ int main(int argc, char *argv[]) {
                 if (client[k] > 0 && FD_ISSET(client[k], &read_fds)) {
                     nready--;
                     if ((readret = read(client[k], buf, BUF_SIZE)) > 1) {
-                        log_write("Server received %d bytes data on %d\n",
-                               (int)readret, client[k]);
+                        log_write("Server received %d bytes data on %d\n", (int)readret, client[k]);
 
                         // handle request
                         Request *request = parse(buf, readret, client[k]);
                         char * response = malloc(20000);
 
-                        if (request == NULL) {
-                            log_write("Bad Request. Request cannot be parsed!\n");
-                            strcat(response, HTTP_VERSION);
-                            strcat(response, STATUS_400);
-                            strcat(response, "\r\n");
-                            break;
-                        }
+                        process_http_request(request, response);
 
-                        if (!strcmp(request->http_version, HTTP_VERSION)) {
-                            log_write("HTTP Version %s not supported.", request->http_version);
-                            strcat(response, HTTP_VERSION);
-                            strcat(response, STATUS_505);
-                            strcat(response, "\r\n");
-                            break;
-                        }
-
-                        if (strcmp(request->http_method, "HEAD") == 0) {
-                            fprintf(stdout, "encountered a HEAD request\n");
-                            process_head(request, response);
-                        }
-                        else if (strcmp(request->http_method, "GET") == 0) {
-                            fprintf(stdout, "encountered a GET request\n");
-                            process_get(request, response);
-                        }
-                        else if (strcmp(request->http_method, "POST") == 0) {
-                            fprintf(stdout, "encountered a POST request\n");
-                            process_post(request, response);
-                        }
-                        else {
-                            fprintf(stdout, "Response with 501");
-                            strcat(response, HTTP_VERSION);
-                            strcat(response, STATUS_501);
-                            strcat(response, "\r\n");
-                        }
-                        printf("response size is %lu\n", strlen(response));
                         printf("response is %s\n", response);
                         if (send(client[k], response, strlen(response), 0) < 0) {
                             close_socket(client[k]);
@@ -342,8 +137,7 @@ int main(int argc, char *argv[]) {
                             log_write("Error sending to client.\n");
                             exit(EXIT_FAILURE);
                         }
-                        log_write("Server sent %d bytes data to %d\n",
-                               strlen(response), client[k]);
+                        log_write("Server sent %d bytes data to %d\n", strlen(response), client[k]);
                         free(response);
                     } else {
                         if (readret == 0) {
